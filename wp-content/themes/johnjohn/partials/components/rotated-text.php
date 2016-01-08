5bbaeeb71617ad2b2d7b0c383dfb46345eaf6de2
Galen Strasen <galen@wearetmbr.com>
2016-01-07 17:24:06 -0800

    rotated text partial

---
+++ wp-content/themes/johnjohn/partials/components/rotated-text.php
@@ -0,0 +1,7 @@
+<div class="wrapper" style="background: pink; position: relative; height: 500px;">
+<section class="rotated-text -right">
+
+    <p class="content -text">Call me old Jack Dusty</p>
+
+</section>
+</div>

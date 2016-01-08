b4369a120316e256082f3b208c64710513f1293a
Galen Strasen <galen@wearetmbr.com>
2016-01-07 17:23:55 -0800

    logo partials

---
+++ wp-content/themes/johnjohn/partials/header/logo-dark.php
@@ -0,0 +1,15 @@
+<div class="logo-wrapper -absolute">
+  <h2 class="logo">
+    <span class="letter">J</span>
+    <span class="letter">o</span>
+    <span class="letter">h</span>
+    <span class="letter">n</span>
+  </h2>
+  <div class="divider"></div>
+  <h2 class="logo -flipped">
+    <span class="letter">J</span>
+    <span class="letter">o</span>
+    <span class="letter">h</span>
+    <span class="letter">n</span>
+  </h2>
+</div><!-- /logo-wrapper -->

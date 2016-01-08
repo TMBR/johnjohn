21651a6caa9c851099a6bbe33e8dda82014d242c
Galen Strasen <galen@wearetmbr.com>
2016-01-07 17:25:19 -0800

    Same div on front page

--- wp-content/themes/johnjohn/front-page.php
+++ wp-content/themes/johnjohn/front-page.php
@@ -4,13 +4,17 @@
 // Hero Slider
 get_template_part( 'xpartials/slider/home-hero' ); ?>
 
-
 	<div class="container">
 
 		<div class="row">
 
 				<div class="col-sm-12">
+<div style="background: gray; height: 1000px; width: 100%; margin-top: 200px;">
 
+
+
+
+</div>
 
 				</div><!-- /col -->
 

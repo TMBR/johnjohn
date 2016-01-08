c80f1bcaa7e74d9bdb0d53cad94fd658ab5cb9cd
Galen Strasen <galen@wearetmbr.com>
2016-01-07 17:24:22 -0800

    sidenav

---
+++ wp-content/themes/johnjohn/partials/global/side-nav.php
@@ -0,0 +1,12 @@
+<section class="rotated-text -fixed -left">
+
+    <ul class="content -menu">
+      <li><a href="#">About</a></li>
+      <li><a href="#">Gear</a></li>
+      <li><a href="#">News</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+
+</section>
+
+

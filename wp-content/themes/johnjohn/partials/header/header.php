9d1777f47dc222f73f984ca23f54d357f5edf369
Galen Strasen <galen@wearetmbr.com>
2016-01-08 15:47:45 -0800

    New partials

---
+++ wp-content/themes/johnjohn/partials/header/header.php
@@ -0,0 +1,6 @@
+<header class="page-header">
+  <div class="numberwrap">
+    <p class="numbers">20</p>
+    <p class="numbers">14</p>
+  </div>
+</header>
---
+++ wp-content/themes/johnjohn/partials/hero.php
@@ -0,0 +1,27 @@
+<div class="page-hero -relative">
+
+
+
+   <div class="video-wrapper">
+   <div class="row ">
+   <div class="col-sm-3">
+    <div class="wrapper">
+    <div class="things">
+    <div class="logo-wrapper">
+   <?php get_template_part( 'partials/components/logo' ); ?>
+   </div>
+      <a href="" class="play-button"></a>
+      </div>
+      </div>
+    </div>
+    <div class="col-sm-9">
+      <h1 class="text -absolute">I am<br/> John John<br/> Florence</h1>
+      <a href="" class="handle -absolute">@john_john_florence</a>
+           <div class="reveal">
+            <div class="videocover"></div>
+              <iframe width="100%" height="100%" src="http://www.youtube.com/embed/bTqVqk7FSmY?feature=oembed" frameborder="0" allowfullscreen=""></iframe>
+
+            </div>
+        </div>
+</div>
+      </div></div>

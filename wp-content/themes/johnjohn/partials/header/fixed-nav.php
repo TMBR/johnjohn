7b3b30696778d831db5a673700593ad570790b3a
Galen Strasen <galen@wearetmbr.com>
2016-01-09 18:46:32 -0800

    Animation classes

--- wp-content/themes/johnjohn/partials/header/fixed-nav.php
+++ wp-content/themes/johnjohn/partials/header/fixed-nav.php
@@ -1,10 +1,10 @@
 <section class="rotated-text -fixed -left">
 
-    <ul class="content -menu">
+    <ul class="content -menu animation" data-animation="animation-fade-in-left">
       <li><a href="#">About</a></li>
       <li><a href="#">Gear</a></li>
       <li><a href="#">News</a></li>
       <li><a href="#">Contact</a></li>
     </ul>
-
+    <div class="rule animation" data-animation="animation-fade-in-right"></div>
 </section>
--- wp-content/themes/johnjohn/partials/header/header.php
+++ wp-content/themes/johnjohn/partials/header/header.php
@@ -1,6 +1,6 @@
 <header class="page-header">
   <div class="numberwrap">
-    <p class="numbers">20</p>
-    <p class="numbers">14</p>
+    <p class="numbers animation" data-animation="animation-fade-in-left">20</p>
+    <p class="numbers animation" data-animation="animation-fade-in-left">14</p>
   </div>
 </header>

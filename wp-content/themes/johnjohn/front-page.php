784f3bce345ebf6cd3b0dcf865c0ae5195c18112
Galen Strasen <galen@wearetmbr.com>
2016-01-09 18:34:05 -0800

    Front page markup

--- wp-content/themes/johnjohn/front-page.php
+++ wp-content/themes/johnjohn/front-page.php
@@ -1,8 +1,43 @@
 <?php get_header(); ?>
 
-<?php
-// Hero Slider
-get_template_part( 'xpartials/slider/home-hero' ); ?>
+<?php get_template_part( 'partials/hero' ); ?>
+
+<div class="container-fluid">
+  <div class="row">
+      <div class="col-sm-3">
+        <img src="<?php echo get_template_directory_uri(); ?>/public/images/placeholder.jpg" style="margin-top: 200px;" class="img-responsive" alt="" />
+      </div>
+
+      <div class="col-sm-2">
+        <?php get_template_part( 'partials/intro' ); ?>
+      </div>
+
+      <div class="col-sm-5">
+      <div id="vid-gallery" class="video-gallery flexslider">
+        <ul class="slides">
+
+                <li>
+                    <img src="<?php echo get_template_directory_uri(); ?>/public/images/image2.jpg" alt="" class="img-responsive" />
+
+                </li>
+                <li>
+                    <img src="<?php echo get_template_directory_uri(); ?>/public/images/image3.jpg" alt="" class="img-responsive" />
+
+                </li>
+
+        </ul>
+    </div>
+
+        <div class="content-card -noimg">
+
+            <h3 class="heading">Billibong Pro Tahiti<br/> Faceoff</h3>
+            <div class="content">
+            <p class="text">The Billabong Pro was crazy. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tempus tincidunt enim, in mattis orci viverra ac. Ut eleifend egestas tincidunt. Etiam bibendum suscipit augue. Vestibulum malesuada leo id commodo feugiat. Praesent at egestas enim. Ut eleifend egestas tincidunt. Nulla tempus tincidunt enim, in mattis orci viverra ac. Ut eleifend egestas tincidunt. Praesent at egestas enim. Etiam bibendum suscipit augue. </p>
+            <a href="" class="link">Read More</a>
+            </div>
+      </div>
+    </div>
+    </div>
 
 	<div class="container">
 

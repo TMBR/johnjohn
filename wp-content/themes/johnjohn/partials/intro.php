9f55f0af0910d2985ebd8f36a5dee24e76c53a8d
Galen Strasen <galen@wearetmbr.com>
2016-01-09 18:34:59 -0800

    Intro partial

---
+++ wp-content/themes/johnjohn/partials/intro.php
@@ -0,0 +1,6 @@
+<div class="content-card -overlap">
+  <img src="<?php echo get_template_directory_uri(); ?>/public/images/him.jpg" alt="" class="img" />
+  <div class="content">
+    <p class="text">Hey gang, my name is John John Florence. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tempus tincidunt enim, in mattis orci viverra ac. Ut eleifend egestas tincidunt. Etiam bibendum suscipit augue. Vestibulum malesuada leo id commodo feugiat. Praesent at egestas enim. </p>
+  </div>
+</div>

f94633ea2aa38046ddec942cec3af8a08461e4f9
Galen Strasen <galen@wearetmbr.com>
2016-01-09 20:52:33 -0800

    Animation classes

--- wp-content/themes/johnjohn/partials/hero.php
+++ wp-content/themes/johnjohn/partials/hero.php
@@ -10,7 +10,7 @@
             <?php get_template_part( 'partials/components/logo' ); ?>
           </div><!-- .logo-wrapper -->
 
-          <a href="" class="play-button"></a>
+          <a href="" class="play-button -animated" id="hero-play"></a>
 
         </div><!-- .leftwrap -->
       </div><!-- /col -->
@@ -18,14 +18,14 @@
       <div class="col-sm-9 _unpad-l">
 
         <h1 class="text -absolute">
-          <span class="line">I am</span>
-          <span class="line">John John</span>
-          <span class="line">Florence</span>
+          <span class="line animation" data-animation="animation-fade-in-left">I am</span>
+          <span class="line animation" data-animation="animation-fade-in-left" data-delay="300">John John</span>
+          <span class="line animation" data-animation="animation-fade-in-left" data-delay="600">Florence</span>
         </h1>
 
         <a href="" class="handle -absolute">@john_john_florence</a>
 
-        <div class="reveal">
+        <div class="reveal animation" data-animation="animation-fade-in-up">
           <div class="videocover"></div>
           <iframe width="100%" height="100%" id="herovid" src="http://www.youtube.com/embed/bTqVqk7FSmY?feature=oembed" frameborder="0" allowfullscreen=""></iframe>
         </div><!-- /reveal -->
--- wp-content/themes/johnjohn/partials/intro.php
+++ wp-content/themes/johnjohn/partials/intro.php
@@ -1,4 +1,4 @@
-<div class="content-card -overlap animation" data-animation="animation-fade-in-down">
+<div id="push-down" class="content-card -overlap animation" data-animation="animation-fade-in-down">
   <img src="<?php echo get_template_directory_uri(); ?>/public/images/him.jpg" alt="" class="img" />
   <div class="content">
     <p class="text">Hey gang, my name is John John Florence. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tempus tincidunt enim, in mattis orci viverra ac. Ut eleifend egestas tincidunt. Etiam bibendum suscipit augue. Vestibulum malesuada leo id commodo feugiat. Praesent at egestas enim. </p>

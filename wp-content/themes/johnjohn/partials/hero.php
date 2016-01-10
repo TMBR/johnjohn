7f39d4f7d7231204805ebfeae32bf6157b2b7f3f
Galen Strasen <galen@wearetmbr.com>
2016-01-09 18:34:30 -0800

    Hero markup

--- wp-content/themes/johnjohn/partials/hero.php
+++ wp-content/themes/johnjohn/partials/hero.php
@@ -1,27 +1,37 @@
+
 <div class="page-hero -relative">
+  <div class="container-fluid">
+    <div class="row">
 
+      <div class="col-sm-3 -heroheight">
+        <div class="leftwrap">
 
+          <div class="logo-wrapper">
+            <?php get_template_part( 'partials/components/logo' ); ?>
+          </div><!-- .logo-wrapper -->
 
-   <div class="video-wrapper">
-   <div class="row ">
-   <div class="col-sm-3">
-    <div class="wrapper">
-    <div class="things">
-    <div class="logo-wrapper">
-   <?php get_template_part( 'partials/components/logo' ); ?>
-   </div>
-      <a href="" class="play-button"></a>
-      </div>
-      </div>
-    </div>
-    <div class="col-sm-9">
-      <h1 class="text -absolute">I am<br/> John John<br/> Florence</h1>
-      <a href="" class="handle -absolute">@john_john_florence</a>
-           <div class="reveal">
-            <div class="videocover"></div>
-              <iframe width="100%" height="100%" src="http://www.youtube.com/embed/bTqVqk7FSmY?feature=oembed" frameborder="0" allowfullscreen=""></iframe>
+          <a href="" class="play-button"></a>
 
-            </div>
-        </div>
-</div>
-      </div></div>
+        </div><!-- .leftwrap -->
+      </div><!-- /col -->
+
+      <div class="col-sm-9 _unpad-l">
+
+        <h1 class="text -absolute">
+          <span class="line">I am</span>
+          <span class="line">John John</span>
+          <span class="line">Florence</span>
+        </h1>
+
+        <a href="" class="handle -absolute">@john_john_florence</a>
+
+        <div class="reveal">
+          <div class="videocover"></div>
+          <iframe width="100%" height="100%" id="herovid" src="http://www.youtube.com/embed/bTqVqk7FSmY?feature=oembed" frameborder="0" allowfullscreen=""></iframe>
+        </div><!-- /reveal -->
+
+      </div><!-- .col -->
+    </div><!-- /row -->
+
+  </div><!-- /container -->
+</div><!-- /page-hero -->

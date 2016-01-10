8a976dfbf849967bcbbcb4beb7a5f6893efd26b0
Galen Strasen <galen@wearetmbr.com>
2016-01-09 18:52:03 -0800

    Animation classes & adjustments

--- wp-content/themes/johnjohn/partials/components/logo.php
+++ wp-content/themes/johnjohn/partials/components/logo.php
@@ -1,13 +1,17 @@
-<h2 class="logo">
-  <span class="letter">J</span>
-  <span class="letter">o</span>
-  <span class="letter">h</span>
-  <span class="letter">n</span>
-</h2>
-<div class="divider"></div>
-<h2 class="logo -flipped">
-  <span class="letter">J</span>
-  <span class="letter">o</span>
-  <span class="letter">h</span>
-  <span class="letter">n</span>
-</h2>
+<div class="animation" data-animation="animation-fade-in-left">
+  <h2 class="logo">
+    <span class="letter">J</span>
+    <span class="letter">o</span>
+    <span class="letter">h</span>
+    <span class="letter">n</span>
+  </h2>
+</div>
+<div class="divider animation" data-animation="animation-fade-in-left"></div>
+<div class="animation" data-animation="animation-fade-in-right">
+  <h2 class="logo -flipped">
+    <span class="letter">J</span>
+    <span class="letter">o</span>
+    <span class="letter">h</span>
+    <span class="letter">n</span>
+  </h2>
+</div>

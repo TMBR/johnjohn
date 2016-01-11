d075ea03e8bd57407949db6b8c9b2fc2e2867c40
Galen Strasen <galen@wearetmbr.com>
2016-01-11 12:39:06 -0800

    New partial

---
+++ wp-content/themes/johnjohn/partials/vid-gallery.php
@@ -0,0 +1,27 @@
+<div class="animation" data-animation="animation-fade-in-up">
+<div id="vid-gallery" class="video-gallery flexslider -navvert">
+  <ul class="slides">
+    <li>
+      <a href="" class="vid-wrap -center">
+        <button class="play-button"></button>
+        <img src="<?php echo get_template_directory_uri(); ?>/public/images/image2.jpg" alt="" class="img-responsive" />
+      </a>
+    </li>
+
+    <li>
+      <a href="" class="vid-wrap -center">
+        <button class="play-button"></button>
+        <img src="<?php echo get_template_directory_uri(); ?>/public/images/image3.jpg" alt="" class="img-responsive" />
+      </a>
+    </li>
+
+    <li>
+      <a href="" class="vid-wrap -center">
+        <button class="play-button"></button>
+        <img src="<?php echo get_template_directory_uri(); ?>/public/images/image2.jpg" alt="" class="img-responsive" />
+      </a>
+    </li>
+
+  </ul>
+</div>
+</div>

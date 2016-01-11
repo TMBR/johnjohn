c8a52343ebe5ea3435f13756de9627c691048d00
Galen Strasen <galen@wearetmbr.com>
2016-01-11 14:02:03 -0800

    Section 2 & 3 breakout

--- wp-content/themes/johnjohn/partials/s2.php
+++ wp-content/themes/johnjohn/partials/s2.php
@@ -1,6 +1,6 @@
 <div class="container-fluid">
     <div class="row">
-      <div class="col-sm-3">
+      <div class="col-sm-3 col-sm-offset-1">
         <img src="<?php echo get_template_directory_uri(); ?>/public/images/placeholder.jpg" style="margin-top: 200px;" class="img-responsive" alt="" />
       </div>
 
---
+++ wp-content/themes/johnjohn/partials/s3.php
@@ -0,0 +1,20 @@
+<div class="large-video -relative animation" data-animation="animation-fade-in-up">
+
+  <section class="rotated-text -right -center">
+    <p class="content -text">Call me old Jack Dusty</p>
+    <div class="rule"></div>
+  </section>
+
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-sm-10 col-sm-offset-1">
+        <a href="" class="vid-wrap -special">
+          <button class="play-button"></button>
+          <div class="surfer"></div>
+          <img src="<?php echo get_template_directory_uri(); ?>/public/images/no-jj-vid-still.jpg" alt="" />
+        </a><!-- vid-wrap -->
+      </div><!-- col -->
+    </div><!-- row -->
+  </div><!-- /container -->
+
+</div><!-- .-relative -->

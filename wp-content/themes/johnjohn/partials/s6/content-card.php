6739891bc071aa41c4b7df91f3b18523bd6c53b7
Galen Strasen <galen@wearetmbr.com>
2016-01-12 15:54:59 -0800

    Animation classes

--- wp-content/themes/johnjohn/partials/s6/content-card.php
+++ wp-content/themes/johnjohn/partials/s6/content-card.php
@@ -1,5 +1,5 @@
-<div class="content-card">
+<div class="content-card animation" data-animation="animation-fade-in-down">
   <h3 class="heading animation" data-animation="animation-fade-in-down">Day 29<br/> Tahiti Session</h3>
-  <p class="text animation" data-animation="animation-fade-in-up">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tempus tincidunt enim, in mattis orci viverra ac. Ut eleifend egestas tincidunt. Etiam bibendum suscipit augue. Vestibulum malesuada leo id commodo feugiat. Praesent at egestas enim. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tempus tincidunt enim, in mattis orci viverra ac. Ut eleifend egestas tincidunt. Etiam bibendum suscipit augue. Vestibulum malesuada leo id commodo feugiat. Praesent at egestas enim.</p>
-  <a href="" class="line-link animation" data-animation="animation-fade-in-up">Day 22</a>
+  <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tempus tincidunt enim, in mattis orci viverra ac. Ut eleifend egestas tincidunt. Etiam bibendum suscipit augue. Vestibulum malesuada leo id commodo feugiat. Praesent at egestas enim. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tempus tincidunt enim, in mattis orci viverra ac. Ut eleifend egestas tincidunt. Etiam bibendum suscipit augue. Vestibulum malesuada leo id commodo feugiat. Praesent at egestas enim.</p>
+  <a href="" class="line-link">Day 22</a>
 </div>

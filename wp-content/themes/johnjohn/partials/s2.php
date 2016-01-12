25da48bb7b410241d3a5d7ad6d8995d69a7562a0
Galen Strasen <galen@wearetmbr.com>
2016-01-11 16:24:52 -0800

    Content card partials

--- wp-content/themes/johnjohn/partials/s2.php
+++ wp-content/themes/johnjohn/partials/s2.php
@@ -6,18 +6,13 @@
       </div>
 
       <div class="col-sm-2">
-        <?php get_template_part( 'partials/s2/intro' ); ?>
+        <?php get_template_part( 'partials/s2/-overlap-content-card' ); ?>
       </div>
 
       <div class="col-sm-5">
 
         <?php get_template_part( 'partials/s2/vid-gallery' ); ?>
-
-        <div class="content-card">
-          <h3 class="heading">Billibong Pro Tahiti<br/> Faceoff</h3>
-          <p class="text">The Billabong Pro was crazy. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tempus tincidunt enim, in mattis orci viverra ac. Ut eleifend egestas tincidunt. Etiam bibendum suscipit augue. Vestibulum malesuada leo id commodo feugiat. Praesent at egestas enim. Ut eleifend egestas tincidunt. Nulla tempus tincidunt enim, in mattis orci viverra ac. Ut eleifend egestas tincidunt. Praesent at egestas enim. Etiam bibendum suscipit augue. </p>
-          <a href="#" class="link">Read More</a>
-        </div><!-- .content-card -->
+        <?php get_template_part( 'partials/s2/content-card' ); ?>
 
       </div><!-- .col -->
 
---
+++ wp-content/themes/johnjohn/partials/s2/content-card.php
@@ -0,0 +1,5 @@
+<div class="content-card">
+  <h3 class="heading">Billibong Pro Tahiti<br/> Faceoff</h3>
+  <p class="text">The Billabong Pro was crazy. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tempus tincidunt enim, in mattis orci viverra ac. Ut eleifend egestas tincidunt. Etiam bibendum suscipit augue. Vestibulum malesuada leo id commodo feugiat. Praesent at egestas enim. Ut eleifend egestas tincidunt. Nulla tempus tincidunt enim, in mattis orci viverra ac. Ut eleifend egestas tincidunt. Praesent at egestas enim. Etiam bibendum suscipit augue. </p>
+  <a href="#" class="link">Read More</a>
+</div><!-- .content-card -->

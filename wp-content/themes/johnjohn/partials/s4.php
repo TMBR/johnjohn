f4843f0fd4356d90128762a40e259e52581e9cb5
Galen Strasen <galen@wearetmbr.com>
2016-01-11 18:40:57 -0800

    Vert caption variants

--- wp-content/themes/johnjohn/partials/s4.php
+++ wp-content/themes/johnjohn/partials/s4.php
@@ -10,7 +10,7 @@
 
   <div class="row">
     <div class="col-sm-6">
-      <div class="vert-caption">
+      <div class="vert-caption -left -overlay">
         <p class="caption -white">Jack Dusty</p>
         <img src="<?php echo get_template_directory_uri(); ?>/public/images/jj.jpg" class="img-responsive" alt="" />
       </div>

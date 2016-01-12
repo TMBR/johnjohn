9b6c64c9e21711977796ce3d70cc680a26e2a5de
Galen Strasen <galen@wearetmbr.com>
2016-01-11 19:52:27 -0800

    Line link styles

--- wp-content/themes/johnjohn/partials/s6/content-card.php
+++ wp-content/themes/johnjohn/partials/s6/content-card.php
@@ -1,5 +1,5 @@
 <div class="content-card">
   <h3 class="heading">Day 29<br/> Tahiti Session</h3>
   <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tempus tincidunt enim, in mattis orci viverra ac. Ut eleifend egestas tincidunt. Etiam bibendum suscipit augue. Vestibulum malesuada leo id commodo feugiat. Praesent at egestas enim. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tempus tincidunt enim, in mattis orci viverra ac. Ut eleifend egestas tincidunt. Etiam bibendum suscipit augue. Vestibulum malesuada leo id commodo feugiat. Praesent at egestas enim.</p>
-  <a href="" class="link">Day 22</a>
+  <a href="" class="line-link">Day 22</a>
 </div>

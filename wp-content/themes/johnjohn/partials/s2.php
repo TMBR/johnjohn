cef9193ee0aa65d2a8905ab8d9f4a5ee814ac8da
Galen Strasen <galen@wearetmbr.com>
2016-01-11 16:06:01 -0800

    Removed unnecessary variant and cleaned up formatting

--- wp-content/themes/johnjohn/partials/s2.php
+++ wp-content/themes/johnjohn/partials/s2.php
@@ -9,14 +9,15 @@
       </div>
 
       <div class="col-sm-5">
+
         <?php get_template_part( 'partials/s2/vid-gallery' ); ?>
-        <div class="content-card -noimg">
+
+        <div class="content-card">
           <h3 class="heading">Billibong Pro Tahiti<br/> Faceoff</h3>
+          <p class="text">The Billabong Pro was crazy. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tempus tincidunt enim, in mattis orci viverra ac. Ut eleifend egestas tincidunt. Etiam bibendum suscipit augue. Vestibulum malesuada leo id commodo feugiat. Praesent at egestas enim. Ut eleifend egestas tincidunt. Nulla tempus tincidunt enim, in mattis orci viverra ac. Ut eleifend egestas tincidunt. Praesent at egestas enim. Etiam bibendum suscipit augue. </p>
+          <a href="#" class="link">Read More</a>
+        </div><!-- .content-card -->
 
-            <p class="text">The Billabong Pro was crazy. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tempus tincidunt enim, in mattis orci viverra ac. Ut eleifend egestas tincidunt. Etiam bibendum suscipit augue. Vestibulum malesuada leo id commodo feugiat. Praesent at egestas enim. Ut eleifend egestas tincidunt. Nulla tempus tincidunt enim, in mattis orci viverra ac. Ut eleifend egestas tincidunt. Praesent at egestas enim. Etiam bibendum suscipit augue. </p>
-            <a href="" class="link">Read More</a>
-
-        </div>
-      </div>
-    </div>
-  </div>
+      </div><!-- .col -->
+    </div><!-- .row -->
+  </div><!-- .container -->

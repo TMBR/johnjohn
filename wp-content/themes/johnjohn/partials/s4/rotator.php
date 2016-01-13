e34cd8465919fe173cf9f8df016b2efd77ae3507
Galen Strasen <galen@wearetmbr.com>
2016-01-13 15:22:41 -0800

    Rotator markup

--- wp-content/themes/johnjohn/partials/s4/rotator.php
+++ wp-content/themes/johnjohn/partials/s4/rotator.php
@@ -1,12 +1,12 @@
-<div class="rotator -overlap">
-  <div class="spinner-wrapper">
-    <img id="spin-img" src="<?php echo get_template_directory_uri(); ?>/public/images/spin/img15.jpg" width="250" alt="" />
+<div class="rotator -horiz -overlap">
+  <div class="img-wrap">
+    <img id="spin-img" src="<?php echo get_template_directory_uri(); ?>/public/images/prod2/img15.jpg" width="250" alt="" />
   </div>
   <div class="prodinfo">
     <img src="<?php echo get_template_directory_uri(); ?>/public/images/hurley.png" alt="" class="brand" />
-    <h4>Hurley Phantom</h4>
+    <h4 class="caption">Hurley Phantom</h4>
   </div><!-- /label -->
   <div id="slider-range"></div>
-  <div id="slider-range-value"></div>
-  <h6>360</h6>
+
+  <h6 class="descrip">360</h6>
 </div>
---
+++ wp-content/themes/johnjohn/partials/s6/rotator.php
@@ -0,0 +1,26 @@
+
+<div id="spinner2" class="rotatorx rotator -vert">
+
+  <div class="img-wrap">
+    <img id="vert-img2" src="<?php echo get_template_directory_uri(); ?>/public/images/prod3/img7.jpg" style="max-width: 90%;"alt="" />
+  </div><!-- /img-wrap -->
+
+  <div class="range-wrap">
+
+    <div class="ranger-overlay" id="hide-slider2">
+      <div class="line"></div>
+      <div class="handle"></div>
+    </div><!-- /ranger-overlay -->
+
+    <div id="vert-slider2"></div>
+
+    <div class="vert-caption prodinfo -right">
+      <img src="<?php echo get_template_directory_uri(); ?>/public/images/hurley.png" alt="" class="brand" />
+      <h4 class="caption">Chola Hendi</h4>
+    </div><!-- /vert-caption -->
+
+  </div><!-- /range-wrap -->
+
+
+
+</div><!-- /rotator -->

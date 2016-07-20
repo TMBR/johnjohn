e573a1037355345b9137b3c224326963fed27de1
Galen Strasen <galen@wearetmbr.com>
2016-07-20 17:48:40 -0400

    Cleanup

--- wp-content/themes/johnjohn/front-page.php
+++ wp-content/themes/johnjohn/front-page.php
@@ -3,28 +3,27 @@
 get_header(); ?>
 
 
-
-<section class="pg-section s1" >
+<section class="pg-section s1">
 <?php get_template_part( 'partials/s1' ); ?>
 </section>
 
-<section class="pg-section s2" >
+<section class="pg-section s2">
 <?php get_template_part( 'partials/s2' ); ?>
 </section>
 
-<section class="pg-section s3" >
+<section class="pg-section s3">
 <?php get_template_part( 'partials/s3' ); ?>
 </section>
 
-<section class="pg-section s4" style="margin-top: -75px;">
+<section class="pg-section s4">
 <?php get_template_part( 'partials/s4' ); ?>
 </section>
 
-<section class="pg-section s5" >
+<section class="pg-section s5">
 <?php get_template_part( 'partials/s5' ); ?>
 </section>
 
-<section class="pg-section s6" >
+<section class="pg-section s6">
 <?php get_template_part( 'partials/s6' ); ?>
 </section>
 

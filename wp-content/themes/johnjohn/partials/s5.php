478b9beb47a30da38b66ae0530880c50b70c5af1
Galen Strasen <galen@wearetmbr.com>
2016-07-20 15:59:18 -0400

    S5 scroll speed markup

--- wp-content/themes/johnjohn/partials/s5.php
+++ wp-content/themes/johnjohn/partials/s5.php
@@ -1,47 +1,44 @@
 <div class="card-gallery -even-ws">
   <div class="container-fluid">
     <div class="row">
-    <div class="col-xs-9 col-xs-offset-3">
-      <h3 class="broken-text -inline animation" data-animation="animation-fade-in-down">
-      <span class="line animation" data-animation="animation-fade-in-left">Just</span>
-      <span class="line -center animation" data-animation="animation-fade-in-left" data-delay="200">Another</span>
-      <span class="line animation" data-animation="animation-fade-in-left" data-delay="400">Day</span>
-      <span class="strike animation" data-animation="animation-fade-in-down" data-delay="500">29</span>
-      </h3>
-    </div>
+
+      <div class="col-xs-9 col-xs-offset-3">
+        <h3 class="broken-text -inline animation" data-animation="animation-fade-in-down">
+          <span class="line animation" data-animation="animation-fade-in-left">Just</span>
+          <span class="line -center animation" data-animation="animation-fade-in-left" data-delay="200">Another</span>
+          <span class="line animation" data-animation="animation-fade-in-left" data-delay="400">Day</span>
+          <span class="strike animation" data-animation="animation-fade-in-down" data-delay="500">29</span>
+        </h3>
+      </div><!-- /col -->
+
+
       <div class="col-xs-12">
-<div class="-relative">
-      <div class="card-overlay animation" data-animation="animation-fade-in-down">
+        <div class="-relative">
 
-        <div class="content-card">
-          <img src="<?php echo get_template_directory_uri(); ?>/public/images/johnjohn.jpg" alt="" class="img" />
-          <p class="text">Hey gang, my name is John John Florence. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tempus tincidunt enim, in mattis orci viverra ac. </p>
-        </div><!-- .content-card -->
+          <div class="card-overlay animation" data-animation="animation-fade-in-down">
+            <div class="content-card">
 
-      </div><!-- /card-overlay -->
+              <div class="speed-wrap -absolute" data-scroll-speed="9">
+                <img src="<?php echo get_template_directory_uri(); ?>/public/images/johnjohn.jpg" alt="" class="img" />
+              </div><!-- /scroll-speed -->
 
-<div class="flexslider -navoverlay animation" data-animation="animation-fade-in-right">
-  <ul class="slides">
-    <li>
+              <p class="text">Hey gang, my name is John John Florence. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tempus tincidunt enim, in mattis orci viverra ac. </p>
+            </div><!-- .content-card -->
+          </div><!-- /card-overlay -->
 
-        <img src="<?php echo get_template_directory_uri(); ?>/public/images/slide1.jpg" alt="" class="img-responsive" />
+          <div class="flexslider -navoverlay animation" data-animation="animation-fade-in-right">
+            <ul class="slides">
+              <li><img src="<?php echo get_template_directory_uri(); ?>/public/images/slide1.jpg" alt="" class="img-responsive" /></li>
 
-    </li>
+              <li><img src="<?php echo get_template_directory_uri(); ?>/public/images/slide2.jpg" alt="" class="img-responsive" /></li>
 
-    <li>
-      <img src="<?php echo get_template_directory_uri(); ?>/public/images/slide2.jpg" alt="" class="img-responsive" />
-    </li>
+              <li><img src="<?php echo get_template_directory_uri(); ?>/public/images/slide3.jpg" alt="" class="img-responsive" /></li>
+            </ul><!-- /slides -->
+          </div><!-- /flexslider -->
 
-    <li>
+        </div><!-- relative -->
+      </div><!-- /col -->
 
-        <img src="<?php echo get_template_directory_uri(); ?>/public/images/slide3.jpg" alt="" class="img-responsive" />
-
-    </li>
-
-  </ul>
-</div>
-
-
-</div>
-</div></div>
-</div></div>
+    </div><!-- /row -->
+  </div><!-- /container -->
+</div><!-- /card-gall -->

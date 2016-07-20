57d323de0c7d4179e05a2e2aaba3f36996851da2
Galen Strasen <galen@wearetmbr.com>
2016-07-20 15:52:02 -0400

    S4 scroll speed data attrs

--- wp-content/themes/johnjohn/partials/s4.php
+++ wp-content/themes/johnjohn/partials/s4.php
@@ -3,7 +3,7 @@
   <div class="row">
     <div class="col-sm-5 col-sm-offset-1">
       <?php get_template_part( 'partials/s4/blockquote' ); ?>
-    </div>
+    </div><!-- /col -->
   </div><!-- /row -->
 
   <div class="row">
@@ -11,12 +11,18 @@
       <div class="vert-caption -left -overlay">
         <p class="caption -white">Jack Dusty</p>
         <img src="<?php echo get_template_directory_uri(); ?>/public/images/jj.jpg" class="img-responsive animation" data-animation="animation-fade-in-left" alt="" />
-      </div>
-    </div>
+      </div><!-- /vert-caption -->
+    </div><!-- /col -->
+
     <div class="col-sm-3">
-      <?php get_template_part( 'partials/s4/rotator' ); ?>
+      <div class="ss10" data-scroll-speed="9">
+        <?php get_template_part( 'partials/s4/rotator' ); ?>
+      </div><!-- /scroll speed -->
     </div><!-- /col -->
   </div><!-- /row -->
+
+
+
 
   <div class="row">
 
@@ -24,18 +30,32 @@
       <?php get_template_part( 'partials/s4/content-card' ); ?>
     </div><!-- /col -->
 
-    <div class="col-sm-6">
-      <div class="highlight -no-ws-r animation" data-animation="animation-fade-in-right" style="background-image: url('<?php echo get_template_directory_uri(); ?>/public/images/surfboard.jpg');">
-        <img src="<?php echo get_template_directory_uri(); ?>/public/images/image4.jpg" class="-absolute img-responsive animation" data-animation="animation-fade-in-down" alt="">
-        <h4 class="broken-text animation" data-animation="animation-fade-in-down">
-          <span class="line animation" data-animation="animation-fade-in-left">This</span>
-          <span class="line animation" data-animation="animation-fade-in-left" data-delay="300">Is How</span>
-          <span class="line animation" data-animation="animation-fade-in-left" data-delay="600">We Pyzel</span>
-          <span class="line animation" data-animation="animation-fade-in-left" data-delay="700">Boom</span>
-        </h4>
-      </div>
 
-    </div>
+    <div class="col-sm-6">
+      <div class="-mt200">
+
+
+        <div class="highlight -no-ws-r animation" data-animation="animation-fade-in-right" style="background-image: url('<?php echo get_template_directory_uri(); ?>/public/images/surfboard.jpg');">
+
+          <div data-scroll-speed="5">
+            <img src="<?php echo get_template_directory_uri(); ?>/public/images/image4.jpg" class="-absolute img-responsive animation" data-animation="animation-fade-in-down" alt="">
+          </div><!-- /scroll speed -->
+
+          <div class="speed-wrap -absolute" data-scroll-speed="9">
+            <h4 class="broken-text animation -relative" data-animation="animation-fade-in-down">
+              <span class="line animation" data-animation="animation-fade-in-left">This</span>
+              <span class="line animation" data-animation="animation-fade-in-left" data-delay="300">Is How</span>
+              <span class="line animation" data-animation="animation-fade-in-left" data-delay="600">We Pyzel</span>
+              <span class="line animation" data-animation="animation-fade-in-left" data-delay="700">Boom</span>
+            </h4>
+          </div><!-- /scroll speed -->
+
+        </div><!-- /highlight -->
+
+
+      </div><!-- mt200 -->
+    </div><!-- /col -->
+
   </div><!-- /row -->
 
 

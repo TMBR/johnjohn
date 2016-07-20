030817944aecb09750540efaa4145492e787efc0
Galen Strasen <galen@wearetmbr.com>
2016-07-20 15:08:34 -0400

    S1 scroll speed data-attrs

--- wp-content/themes/johnjohn/partials/s1.php
+++ wp-content/themes/johnjohn/partials/s1.php
@@ -10,10 +10,11 @@
             <?php get_template_part( 'partials/global/logo' ); ?>
           </div><!-- .logo-wrapper -->
 
-          <button class="play-button -animated" id="hero-play" data-animation="animation-fade-in-down"></button>
+          <button class="play-button -animated" id="hero-play" data-scroll-speed="4" data-animation="animation-fade-in-down"></button>
 
         </div><!-- .leftwrap -->
       </div><!-- /col -->
+
 
       <div class="col-sm-9 _unpad-l">
 
@@ -25,10 +26,12 @@
 
         <a href="https://www.instagram.com/john_john_florence/?hl=en" target="_blank" class="handle -absolute animation" data-animation="animation-fade-in-left" data-delay="300">@john_john_florence</a>
 
-        <div class="reveal animation" data-animation="animation-fade-in-up">
-          <div class="videocover"></div>
-          <iframe width="100%" height="100%" id="herovid" data-src="http://www.youtube.com/embed/bTqVqk7FSmY" src="http://www.youtube.com/embed/bTqVqk7FSmY?feature=oembed" frameborder="0" allowfullscreen=""></iframe>
-        </div><!-- /reveal -->
+        <div data-scroll-speed="9">
+          <div class="reveal animation" data-animation="animation-fade-in-up">
+            <div class="videocover"></div>
+            <iframe width="100%" height="100%" id="herovid" data-src="http://www.youtube.com/embed/bTqVqk7FSmY" src="http://www.youtube.com/embed/bTqVqk7FSmY?feature=oembed" frameborder="0" allowfullscreen=""></iframe>
+          </div><!-- /reveal -->
+        </div><!-- /scroll-speed -->
 
       </div><!-- .col -->
     </div><!-- /row -->

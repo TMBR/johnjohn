8844433e5bfc3be7ad41d4e86612b81363b689dc
Galen Strasen <galen@wearetmbr.com>
2016-07-20 15:09:43 -0400

    S2 scroll speed data-attrs

--- wp-content/themes/johnjohn/partials/s2.php
+++ wp-content/themes/johnjohn/partials/s2.php
@@ -1,19 +1,25 @@
 <div class="hero-overlap">
   <div class="container-fluid">
     <div class="row">
+
+
       <div class="col-sm-3 col-sm-offset-1">
-        <?php get_template_part( 'partials/s2/rotator' ); ?>
-      </div>
+        <div data-scroll-speed="5">
+          <?php get_template_part( 'partials/s2/rotator' ); ?>
+        </div><!-- scroll speed -->
+      </div><!-- /col -->
+
 
       <div class="col-sm-2">
-        <?php get_template_part( 'partials/s2/-overlap-content-card' ); ?>
-      </div>
+        <div data-scroll-speed="6">
+          <?php get_template_part( 'partials/s2/-overlap-content-card' ); ?>
+        </div><!-- scroll speed -->
+      </div><!-- /col -->
+
 
       <div class="col-sm-5">
-
         <?php get_template_part( 'partials/s2/vid-gallery' ); ?>
         <?php get_template_part( 'partials/s2/content-card' ); ?>
-
       </div><!-- .col -->
 
     </div><!-- .row -->

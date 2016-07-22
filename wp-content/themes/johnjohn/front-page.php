b8f290e5b513cdfc1c27f8d27e0f58c109ccaff8
Galen Strasen <galen@wearetmbr.com>
2016-07-22 17:01:21 -0400

    classes to sections

--- wp-content/themes/johnjohn/front-page.php
+++ wp-content/themes/johnjohn/front-page.php
@@ -3,27 +3,27 @@
 get_header(); ?>
 
 
-<section class="pg-section s1">
+<section class="pg-section s1 js-section js-s1" data-section="pg-s1">
 <?php get_template_part( 'partials/s1' ); ?>
 </section>
 
-<section class="pg-section s2">
+<section class="pg-section s2 js-section js-s2" data-section="pg-s2">
 <?php get_template_part( 'partials/s2' ); ?>
 </section>
 
-<section class="pg-section s3">
+<section class="pg-section s3 js-section js-s3" data-section="pg-s3">
 <?php get_template_part( 'partials/s3' ); ?>
 </section>
 
-<section class="pg-section s4">
+<section class="pg-section s4 js-section js-s4" data-section="pg-s4">
 <?php get_template_part( 'partials/s4' ); ?>
 </section>
 
-<section class="pg-section s5">
+<section class="pg-section s5 js-section js-s5" data-section="pg-s5">
 <?php get_template_part( 'partials/s5' ); ?>
 </section>
 
-<section class="pg-section s6">
+<section class="pg-section s6 js-section js-s1" data-section="pg-s6">
 <?php get_template_part( 'partials/s6' ); ?>
 </section>
 

28910e7188378f8068672072cdaf1c6d7c7d294c
Galen Strasen <galen@wearetmbr.com>
2016-07-25 18:39:55 -0400

    Video urls

--- wp-content/themes/johnjohn/partials/s2/vid-gallery.php
+++ wp-content/themes/johnjohn/partials/s2/vid-gallery.php
@@ -2,21 +2,21 @@
   <div class="video-gallery flexslider -navvert">
     <ul class="slides">
       <li>
-        <a href="https://www.youtube.com/watch?v=OnqnCoPLdyw" class="vid-wrap -center js-popupvideo">
+        <a href="https://vimeo.com/ondemand/viewfromabluemoon/148501600" class="vid-wrap -center js-popupvideo">
           <button class="play-button"></button>
           <img src="<?php echo get_template_directory_uri(); ?>/public/images/image2.jpg" alt="" class="img-responsive" />
         </a>
       </li>
 
       <li>
-        <a href="https://www.youtube.com/watch?v=OnqnCoPLdyw" class="vid-wrap -center js-popupvideo">
+        <a href="https://vimeo.com/70917727" class="vid-wrap -center js-popupvideo">
           <button class="play-button"></button>
           <img src="<?php echo get_template_directory_uri(); ?>/public/images/image3.jpg" alt="" class="img-responsive" />
         </a>
       </li>
 
       <li>
-        <a href="https://www.youtube.com/watch?v=OnqnCoPLdyw" class="vid-wrap -center js-popupvideo">
+        <a href="https://vimeo.com/97692400" class="vid-wrap -center js-popupvideo">
           <button class="play-button"></button>
           <img src="<?php echo get_template_directory_uri(); ?>/public/images/image2.jpg" alt="" class="img-responsive" />
         </a>
--- wp-content/themes/johnjohn/partials/s3.php
+++ wp-content/themes/johnjohn/partials/s3.php
@@ -6,7 +6,7 @@
     <div class="row">
       <div class="col-sm-10 col-sm-offset-1">
 
-        <a href="" class="vid-wrap -special">
+        <a href="https://vimeo.com/70917727" class="vid-wrap -special js-popupvideo">
           <button class="play-button"></button>
           <div class="surfer"></div>
           <img src="<?php echo get_template_directory_uri(); ?>/public/images/no-jj-vid-still.jpg" alt="" />

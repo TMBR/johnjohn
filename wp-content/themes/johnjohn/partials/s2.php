04c4f11801f919de31fe9b88d23c94ff9a72d474
Galen Strasen <galen@wearetmbr.com>
2016-07-25 17:03:48 -0400

    Col-md - s2

--- wp-content/themes/johnjohn/partials/s2.php
+++ wp-content/themes/johnjohn/partials/s2.php
@@ -2,22 +2,25 @@
   <div class="container-fluid">
     <div class="row">
 
+      <div class="col-md-6">
 
-      <div class="col-sm-3 col-sm-offset-1">
-        <div class="ss-ease" data-scroll-speed="4">
-          <?php get_template_part( 'partials/s2/rotator' ); ?>
-        </div><!-- scroll speed -->
+        <div class="row">
+          <div class="col-lg-4 col-lg-push-8 col-lg-offset-0 col-md-6 col-md-offset-6">
+            <div class="ss-ease" data-scroll-speed="2">
+              <?php get_template_part( 'partials/s2/-overlap-content-card' ); ?>
+            </div><!-- scroll speed -->
+          </div><!-- /col -->
+
+          <div class="col-lg-6 col-lg-offset-2 col-offset-0 col-lg-pull-4 col-md-9 col-md-offset-3">
+            <div class="ss-ease" data-scroll-speed="4">
+              <?php get_template_part( 'partials/s2/rotator' ); ?>
+            </div><!-- scroll speed -->
+          </div><!-- /col -->
+        </div><!-- /row -->
+
       </div><!-- /col -->
 
-
-      <div class="col-sm-2">
-        <div class="ss-ease" data-scroll-speed="2">
-          <?php get_template_part( 'partials/s2/-overlap-content-card' ); ?>
-        </div><!-- scroll speed -->
-      </div><!-- /col -->
-
-
-      <div class="col-sm-5">
+      <div class="col-lg-5 col-md-6">
         <div class="speed-wrap ss-ease" data-scroll-speed="5">
         <?php get_template_part( 'partials/s2/vid-gallery' ); ?>
         <?php get_template_part( 'partials/s2/content-card' ); ?>
